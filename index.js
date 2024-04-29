3c24b0427ba1de712c23254621335a3052868a39
monircom <monircom@gmail.com>
2024-04-29 17:04:36 +0600

    .envr added

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+//Require('Dotenv').Config();
 const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
